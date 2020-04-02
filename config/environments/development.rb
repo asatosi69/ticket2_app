15bd746459d4716ff26025944c4bd99889f972a1
Hashimoto <puente.japan@gmail.com>
2020-04-02 11:38:38 +0900

    timeoutableまで

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,14 @@ Rails.application.configure do
   
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "puente.japan@gmail.com",
+    :password => "zanytnaetbevfeic",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
