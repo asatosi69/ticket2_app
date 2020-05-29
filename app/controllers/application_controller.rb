7f65bb642404c37b32dd6bf7a81910a76f0a1231
Hashimoto <puente.japan@gmail.com>
2020-05-29 22:31:59 +0900

    マスタがない時のナビゲーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :master_record?
 
   protected
     def configure_permitted_parameters
@@ -17,36 +18,6 @@ class ApplicationController < ActionController::Base
         unless current_seller.admin_flag?
             flash[:notice] = "管理取扱者以外の方は操作できません。"
             redirect_to("/pages")
-        end
-    end
-    
-#『公演』モデルにレコードが無い状態で『チケット』関連処理が実行されるのを避けるため、『公演』モデルにレコードあるかの確認
-    def stage_records_empty?
-        @stages = Stage.all
-        
-        if @stages.empty?
-           flash[:notice] = "予約情報の『確認/登録/編集』には、先に『公演』の登録が必須です。"
-           redirect_to("/stages")
-        end
-    end
-    
-#『チケット種別』モデルにレコードが無い状態で『チケット』関連処理が実行されるのを避けるため、『チケット種別』モデルにレコードあるかの確認
-    def kind_records_empty?
-        @kinds = Kind.all
-    
-        if @kinds.empty?
-           flash[:notice] = "予約情報を『確認/登録/編集』には、先に『チケット種別』の登録が必須です。"
-           redirect_to("/kinds")
-        end
-    end
-    
-#『支払方法』モデルにレコードが無い状態で『チケット』関連処理が実行されるのを避けるため、『支払方法』モデルにレコードあるかの確認
-    def payment_records_empty?
-        @payments = Payment.all
-        
-        if @payments.empty?
-            flash[:notice] = "予約情報を『確認/登録/編集』には、先に『支払方法』の登録が必須です。"
-            redirect_to("/payments")
         end
     end
     
@@ -82,19 +53,27 @@ class ApplicationController < ActionController::Base
         end
     end
     
-    # 自分以外の予約は操作できないようにする
-    def correct_user?
-        # 『取扱者』の場合
-        unless current_seller.admin_flag?
-            binding.pry
-            # 自分以外の予約
-            if current_seller.id != ticket.seller_id
-              flash[:notice] = "他の取扱者の予約は操作できません"
-              redirect_to("/tickets")
-            end
-        end
+    # チケットレコードを作成するの必要なマスタ(『公演』/『チケット種別』/『支払方法』)のレコードが存在するのか確認する(register_controller用以外)
+    def master_record?
+        
+        @master_stage = Stage.all
+        @master_kind = Kind.all
+        @master_payment = Payment.all
+        
     end
     
+    # チケットレコードを作成するの必要なマスタ(『公演』/『チケット種別』/『支払方法』)のレコードが存在するのか確認する(register_controller用)
+    def master_record2?
+        
+        @master_stage = Stage.all
+        @master_kind = Kind.all
+        @master_payment = Payment.all
+        
+        if @master_stage.empty? || @master_kind.empty? || @master_payment.empty?
+            redirect_to("http://www.google.co.jp")
+        end
+        
+    end
     
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
