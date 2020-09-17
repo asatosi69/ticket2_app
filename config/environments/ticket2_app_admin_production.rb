dff0f40ee7c114ef52ca9a8afc1a1bc4a0ad0e90
Hashimoto <puente.japan@gmail.com>
2020-09-17 19:25:03 +0900

    ラチェットサブドメイン追加12

--- config/environments/ticket2_app_admin_production.rb
+++ config/environments/ticket2_app_admin_production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { protocol: 'http://', host: 'oshibai.net' }
+  config.action_mailer.default_url_options = { protocol: 'https://', host: 'oshibai.net' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
